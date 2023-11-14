b45e0aa68bfcff0b3581d60090a1ab4e59598206
yahyakhafif <khafif.yh@gmail.com>
2023-11-14 23:16:17 +0200

    shortening functionality

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const ShortUrl = require('./models/shortUrl')
 const app = express()
+
+mongoose.connect('mongodb://localhost/urlShortener')
+
 app.set('view engine', 'ejs')
-const port = 3000
+app.use(express.urlencoded({ extended: false }))
 
-
-
-app.get('/', (res, req) => {
-    res.render('index')
+app.get('/', async (req, res) => {
+    const shortUrls = await ShortUrl.find()
+    res.render('index', { shortUrls: shortUrls })
 })
 
-app.listen(process.env.PORT || port, () => {
-    console.log(`Server is running on port ${port}`)
+app.post('/shortUrls', async (req, res) => {
+    await ShortUrl.create({ full: req.body.fullUrl })
+
+    res.redirect('/')
 })
+
+app.get('/:shortUrl', async (req, res) => {
+    const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+    if (shortUrl == null) return res.sendStatus(404)
+
+    shortUrl.clicks++
+    shortUrl.save()
+
+    res.redirect(shortUrl.full)
+})
+
+app.listen(process.env.PORT || 3000);
